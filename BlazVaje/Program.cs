828d4169e70c9864dbf7fe56a2dd00628774227b
Blaz <Blaz@DESKTOP-DR54ICN>
2017-09-18 21:00:58 +0200

    Write a C# Sharp program to print the output of multiplication of three numbers which will be entered by the user.

--- BlazVaje/Program.cs
+++ BlazVaje/Program.cs
@@ -9,12 +9,18 @@ namespace BlazVaje
     {
         static void Main(string[] args)
         {
-            int x = 5;
-            int y = 6;
-            int z = x;
-            x = y;
-            y = z;
-            Console.WriteLine("X {0}, Y {1}", x, y);
+            int x;
+            Console.WriteLine("Enter a number X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+            Console.WriteLine("Enter a number Y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int z;
+            Console.WriteLine("Enter a number Z: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x * y * z);
             Console.Read();
         }
     }
